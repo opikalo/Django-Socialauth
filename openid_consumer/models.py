237556757ae49ece4a991b7b87d627386cadc63b
Olekiy Pikalo <opikalo@opikalo-ubuntu.(none)>
2011-04-22 14:05:43 -0400

    Modified consumer model to work with django-nonrel.

--- openid_consumer/models.py
+++ openid_consumer/models.py
@@ -9,12 +9,12 @@ class Nonce(models.Model):
         return "Nonce: %s" % self.nonce
 
 class Association(models.Model):
-    server_url = models.TextField(max_length=2047)
+    server_url = models.CharField(max_length=2047)
     handle = models.CharField(max_length=255)
-    secret = models.TextField(max_length=255) # Stored base64 encoded
+    secret = models.CharField(max_length=255) # Stored base64 encoded
     issued = models.IntegerField()
     lifetime = models.IntegerField()
-    assoc_type = models.TextField(max_length=64)
+    assoc_type = models.CharField(max_length=64)
 
     def __unicode__(self):
         return "Association: %s, %s" % (self.server_url, self.handle)
